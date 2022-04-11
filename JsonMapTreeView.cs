6f8e9c8d1d2a8ef91d24dcafa6e50ae660b905cc
pakho <pakho@WSMORV18>
2022-04-11 16:40:24 +0300

    Добавьте файлы проекта.

---
+++ JsonMapTreeView.cs
@@ -0,0 +1,13 @@
+﻿namespace Project_Settings
+{
+    public class MappingConfigTreeView
+    {
+        public SignalMap[] Lists { get; set; }
+        public SignalMap[] ListMsg { get; set; }
+    }
+
+    public class SignalMap
+    {
+        public string Item { get; set; }
+    }
+}
