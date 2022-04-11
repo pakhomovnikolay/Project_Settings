34bba208e40e940c396892257231a58f8967fde2
Николай <pakhomovnikolay@live.com>
2022-04-11 17:38:35 +0300

    Добавляем базовую модель-представления для проекта

---
+++ Infrastructure/Commands/RelayCommand.cs
@@ -0,0 +1,23 @@
+﻿using Project_Settings.Infrastructure.Commands.Default;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Settings.Infrastructure.Commands
+{
+    public class RelayCommand : Command
+    {
+        private readonly Action<object> _Execute;
+        private readonly Func<object, bool> _CanExecute;
+        public RelayCommand(Action<object> Execute, Func<object, bool> CanExecute)
+        {
+            _Execute = Execute ?? throw new ArgumentNullException(nameof(Execute));
+            _CanExecute = CanExecute;
+        }
+
+        public override bool CanExecute(object parameter) => _CanExecute?.Invoke(parameter) ?? true;
+        public override void Execute(object parameter) => _Execute(parameter);
+    }
+}
